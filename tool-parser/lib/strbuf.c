ce39e4536ce968dc745f3363a904e2b5124139f0
Diego Trabazo Sardón <dtrabazo@gmail.com>
2021-10-06 21:28:09 +0200

    Añade licencias individuales

--- tool-parser/lib/strbuf.c
+++ tool-parser/lib/strbuf.c
@@ -1,4 +1,24 @@
 /*
+ * GaliasDoc: information extraction from PDF sources with common templates
+ * Copyright (C) 2021 Alfonso Landín Piñeiro
+ *
+ * This file is part of GaliasDoc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+/*
  * strbuf.c - Un buffer para cadenas de caracteres que crece de tamaño de
  *            manera automática.
  *
